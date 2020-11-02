14180b468f9939b2c2ee0ae6c4d0305e38aa99a8
julian3493 <julianramosarevalo@gmail.com>
2020-11-02 09:12:23 -0500

    create scraper class

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -2,9 +2,31 @@ require 'HTTParty'
 require 'Nokogiri'
 
 class Scraper
-  attr_accessor :parse_page
+
+  attr_accessor :parse_page, :doc
+
   def initialize
-    file = HTTParty.get('http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3')
-    @parse_page = Nokogiri::HTML(file)
+    @doc = HTTParty.get('https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3')
+    @parse_page = Nokogiri::HTML(@doc)
   end
+
+  def names
+    @names = parse_page.css('.grid-item-info').css('.product-name').css('p').children.map { |name| name.text }.compact
+    puts names
+  end
+
+  def prices
+    prices = parse_page.css('.grid-item-info').css('.grid-item-info').css('.product-name').css('span.local').children.map { |price| price.text }.compact
+  end
+
 end
+
+scraper = Scraper.new
+names = scraper.names
+prices = scraper.prices
+
+  (0...prices.size).each do |index|
+    puts 'entra'
+    puts "- - - index #{index + 1} - - -"
+    puts "Name: #{names[index]} | Price: #{prices[index]}"
+  end
