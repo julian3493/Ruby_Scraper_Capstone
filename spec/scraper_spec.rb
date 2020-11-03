537abd819923be9e23eabe5ecae0d99b0a8a724a
julian3493 <julianramosarevalo@gmail.com>
2020-11-03 11:39:31 -0500

    create test case for check_title  method in Scraper class

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -15,9 +15,16 @@ describe Scraper do
       expect { scraper.format_data }.to raise_error(NoMethodError)
     end
   end
+
   describe '#print_data' do
     it 'print the saved data' do
       expect { scraper.print_data }.to output.to_stdout
+    end
+  end
+
+  describe '#check_title' do
+    it 'return TRUE if the Title given is into the data' do
+      expect(scraper.check_title('Munou na Nana')).to eql(TRUE)
     end
   end
 end
