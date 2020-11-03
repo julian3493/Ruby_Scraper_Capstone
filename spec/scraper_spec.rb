ecacdf551283e289947f198d5191898bc6042ae2
julian3493 <julianramosarevalo@gmail.com>
2020-11-03 10:55:49 -0500

    create test case for format_data method in Scraper class

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -2,4 +2,11 @@
 
 require_relative '../lib/scraper.rb'
 
-
+describe Scraper do
+  let(:scraper) { Scraper.new('https://www3.animeflv.net/browse') }
+  describe '#format_data' do
+    it 'create an array with grouped data' do
+      expect(scraper.format_data).to be_a(Array)
+    end
+  end
+end
