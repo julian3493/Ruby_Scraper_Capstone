a4304f4f75b95dd1307027764fa09b906602af52
julian3493 <julianramosarevalo@gmail.com>
2020-11-03 11:15:33 -0500

    create test case for private method

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -5,11 +5,14 @@ require_relative '../lib/scraper.rb'
 describe Scraper do
   let(:scraper) { Scraper.new('https://www3.animeflv.net/browse') }
   describe '#format_data' do
-    it 'create an array with grouped data' do
-      expect(scraper.format_data).to be_a(Array)
-    end
-    it 'create an array of hashes' do
-      expect(scraper.format_data.all? { |item| item.is_a?(Hash) }).to eql(TRUE)
+    # it 'create an array with grouped data' do
+    #   expect(scraper.format_data).to be_a(Array)
+    # end
+    # it 'create an array of hashes' do
+    #   expect(scraper.format_data.all? { |item| item.is_a?(Hash) }).to eql(TRUE)
+    # end
+    it 'raise error for private methods' do
+      expect { scraper.format_data }.to raise_error(NoMethodError)
     end
   end
 end
