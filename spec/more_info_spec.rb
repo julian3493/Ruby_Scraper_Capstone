2869d133e4eb2befc249c997102dac84d6e21ade
julian3493 <julianramosarevalo@gmail.com>
2020-11-03 11:49:34 -0500

    create test case for print_info method in MoreInfo class

---
+++ spec/more_info_spec.rb
@@ -0,0 +1,12 @@
+# enumerable_spec.rb
+
+require_relative '../lib/more_info.rb'
+
+describe MoreInfo do
+  let(:more_info) {MoreInfo.new('https://www3.animeflv.net/anime/black-clover-tv')}
+  describe '#print_info' do
+    it 'print the saved data' do
+      expect { more_info.print_info }.to output.to_stdout
+    end
+  end
+end
